4ae5bdb578de9d8f53cb51ddcdd0d819d06f7afa
Doug Stephen <dstephen@ihmc.us>
2017-01-26 11:32:17 -0600

    Starting with the Robot Description generator.

---
+++ ModelFileLoader/src/us/ihmc/modelFileLoaders/urdfLoader/RobotDescriptionFromURDFLoader.java
@@ -0,0 +1,23 @@
+package us.ihmc.modelFileLoaders.urdfLoader;
+
+import us.ihmc.robotics.partNames.JointNameMap;
+import us.ihmc.robotics.robotDescription.RobotDescription;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
+ */
+public class RobotDescriptionFromURDFLoader
+{
+   public RobotDescriptionFromURDFLoader()
+   {
+
+   }
+
+//   public RobotDescription loadRobotDescriptionFromURDF(String modelName, InputStream inputStream, List<String> resourceDirectories, JointNameMap jointNameMap, boolean useCollisionMeshes)
+//   {
+//      return null;
+//   }
+}
