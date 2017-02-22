919518b30564c3e58b1c6b97177383898d6b1603
Doug Stephen <dstephen@ihmc.us>
2017-02-22 15:44:44 -0600

    Parse link inertial information and populate the Link Description.
    see ihmcrobotics/ihmc-open-robotics-software#81

--- ModelFileLoader/src/us/ihmc/modelFileLoaders/ModelFileLoaderConversionsHelper.java
+++ ModelFileLoader/src/us/ihmc/modelFileLoaders/ModelFileLoaderConversionsHelper.java
@@ -5,47 +5,93 @@ import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.modelFileLoaders.SdfLoader.xmlDescription.SDFInertia;
+import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.*;
 import us.ihmc.robotics.dataStructures.MutableColor;
-import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.URDFJoint;
-import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.URDFLink;
-import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.URDFRobot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ModelFileLoaderConversionsHelper
 {
 
-   public static Map<String, URDFJoint> getAllJointsFromURDFRobot(URDFRobot urdfRobot)
+   /**
+    * <p>Helper method to accomodate the oddities of the JAXB/XJC generated code for URDFRobot.</p>
+    *
+    * <p>The implementation of the XSD schema leads to all top-level {@link URDFLink}, {@link URDFJoint}, {@link URDFMaterialGlobal}, and {@link URDFTransmission}
+    * to be contained in a single list of objects. This method iterates over that list and separates everything out in to a single pass.</p>
+    *
+    * @param urdfRobot The {@link URDFRobot} instance to parse.
+    * @param urdfJointsToPack The map to insert joint information in to. Maps from joint name to joint object.
+    * @param urdfLinksToPack The map to insert link information in to. Maps from link name to link object.
+    * @param urdfGlobalMaterialsToPack The map to insert global material definitions in to. Maps from material name to material object.
+    * @param urdfTransmissionToPack The map to insert transmission information in to. Maps from transmission name to transmission object.
+    */
+   public static void getGlobalLinksJointsMaterialsAndTransmissionFromURDFRobot(URDFRobot urdfRobot, HashMap<String, URDFJoint> urdfJointsToPack,
+                                                                                HashMap<String, URDFLink> urdfLinksToPack,
+                                                                                HashMap<String, URDFMaterialGlobal> urdfGlobalMaterialsToPack,
+                                                                                HashMap<String, URDFTransmission> urdfTransmissionToPack)
    {
-      HashMap<String, URDFJoint> ret = new HashMap<>();
-
       for (Object o : urdfRobot.getJointAndLinkAndMaterial())
       {
-         if(o instanceof URDFJoint)
+         if (o instanceof URDFJoint)
          {
             URDFJoint urdfJoint = (URDFJoint) o;
 
-            ret.put(urdfJoint.getName(), urdfJoint);
+            urdfJointsToPack.put(urdfJoint.getName(), urdfJoint);
          }
-      }
 
-      return ret;
-   }
-
-   public static Map<String, URDFLink> getAllLinksFromURDFRobot(URDFRobot urdfRobot)
-   {
-      HashMap<String, URDFLink> ret = new HashMap<>();
-
-      for (Object o : urdfRobot.getJointAndLinkAndMaterial())
-      {
-         if(o instanceof URDFLink)
+         if (o instanceof URDFLink)
          {
             URDFLink urdfLink = (URDFLink) o;
 
-            ret.put(urdfLink.getName(), urdfLink);
+            urdfLinksToPack.put(urdfLink.getName(), urdfLink);
+         }
+
+         if (o instanceof URDFMaterialGlobal)
+         {
+            URDFMaterialGlobal urdfMaterial = (URDFMaterialGlobal) o;
+
+            urdfGlobalMaterialsToPack.put(urdfMaterial.getName(), urdfMaterial);
+         }
+
+         if (o instanceof URDFTransmission)
+         {
+            URDFTransmission urdfTransmission = (URDFTransmission) o;
+
+            urdfTransmissionToPack.put(urdfTransmission.getName(), urdfTransmission);
+         }
+      }
+   }
+
+   /**
+    * <p>Similarly to {@link ModelFileLoaderConversionsHelper#getGlobalLinksJointsMaterialsAndTransmissionFromURDFRobot(URDFRobot, HashMap, HashMap, HashMap, HashMap)},
+    * this method eases parsing a compount list of Objects that can be one of several types. This method returns the singular instance of {@link URDFInertial} while packing
+    * the {@link URDFVisual}s and {@link URDFCollision}s in to the supplied out-args.
+    *
+    * @param urdfLink The link to process
+    * @param urdfVisualsToPack The collection of visual definitions to pack
+    * @param urdfCollisionsToPack The colleciton of collision defintions to pack
+    * @return The Intertial information for the joint, or null if it is not present
+    */
+   public static URDFInertial getInertialInformationAndPackVisualsAndCollisionsForURDFLink(URDFLink urdfLink, List<URDFVisual> urdfVisualsToPack, List<URDFCollision> urdfCollisionsToPack)
+   {
+      URDFInertial ret = null;
+      for (Object o : urdfLink.getInertialOrVisualOrCollision())
+      {
+         if (o instanceof URDFInertial)
+         {
+            ret = (URDFInertial) o;
+         }
+
+         if(o instanceof URDFVisual)
+         {
+            urdfVisualsToPack.add((URDFVisual) o);
+         }
+
+         if(o instanceof URDFCollision)
+         {
+            urdfCollisionsToPack.add((URDFCollision) o);
          }
       }
 
@@ -58,7 +104,7 @@ public class ModelFileLoaderConversionsHelper
 
       for (URDFLink urdfLink : urdfLinks)
       {
-//         System.out.println(urdfLink.ge);
+         //         System.out.println(urdfLink.ge);
       }
 
       return ret;
@@ -76,14 +122,14 @@ public class ModelFileLoaderConversionsHelper
       return vector3d;
 
    }
-   
+
    public static Vector3D stringToVector3d(String vector)
    {
       String[] vecString = vector.split("\\s+");
       Vector3D vector3d = new Vector3D(Double.parseDouble(vecString[0]), Double.parseDouble(vecString[1]), Double.parseDouble(vecString[2]));
       return vector3d;
    }
-   
+
    public static MutableColor stringToColor(String color)
    {
       String[] vecString = color.split("\\s+");
@@ -91,52 +137,50 @@ public class ModelFileLoaderConversionsHelper
       return color3f;
    }
 
-
-   
    public static Vector2D stringToVector2d(String xy)
    {
       String[] vecString = xy.split("\\s+");
-   
+
       Vector2D vector = new Vector2D(Double.parseDouble(vecString[0]), Double.parseDouble(vecString[1]));
       return vector;
-   
+
    }
 
    public static RigidBodyTransform poseToTransform(String pose)
    {
       RigidBodyTransform ret = new RigidBodyTransform();
-      if(pose == null)
+      if (pose == null)
       {
          return ret;
       }
       pose = pose.trim();
       String[] data = pose.split("\\s+");
-      
+
       RigidBodyTransform translation = new RigidBodyTransform();
       Vector3D translationVector = new Vector3D();
       translationVector.setX(Double.parseDouble(data[0]));
       translationVector.setY(Double.parseDouble(data[1]));
       translationVector.setZ(Double.parseDouble(data[2]));
       translation.setTranslationAndIdentityRotation(translationVector);
-   
+
       RigidBodyTransform rotation = new RigidBodyTransform();
       Vector3D eulerAngels = new Vector3D();
       eulerAngels.setX(Double.parseDouble(data[3]));
       eulerAngels.setY(Double.parseDouble(data[4]));
       eulerAngels.setZ(Double.parseDouble(data[5]));
       rotation.setRotationEulerAndZeroTranslation(eulerAngels);
-   
+
       ret.set(translation);
       ret.multiply(rotation);
-   
+
       return ret;
    }
 
    public static Matrix3D sdfInertiaToMatrix3d(SDFInertia sdfInertia)
    {
-      
+
       Matrix3D inertia = new Matrix3D();
-      if(sdfInertia != null)
+      if (sdfInertia != null)
       {
          double ixx = Double.parseDouble(sdfInertia.getIxx());
          double ixy = Double.parseDouble(sdfInertia.getIxy());
@@ -156,5 +200,4 @@ public class ModelFileLoaderConversionsHelper
       }
       return inertia;
    }
-
 }
--- ModelFileLoader/src/us/ihmc/modelFileLoaders/urdfLoader/RobotDescriptionFromURDFLoader.java
+++ ModelFileLoader/src/us/ihmc/modelFileLoaders/urdfLoader/RobotDescriptionFromURDFLoader.java
@@ -1,12 +1,14 @@
 package us.ihmc.modelFileLoaders.urdfLoader;
 
+import us.ihmc.euclid.matrix.Matrix3D;
+import us.ihmc.euclid.matrix.RotationMatrix;
 import us.ihmc.modelFileLoaders.ModelFileLoaderConversionsHelper;
-import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.URDFJoint;
-import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.URDFLink;
-import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.URDFRobot;
+import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.*;
 import us.ihmc.modelFileLoaders.urdfLoader.xmlDescription.extensions.URDFGazebo;
 import us.ihmc.robotics.partNames.JointNameMap;
+import us.ihmc.robotics.robotDescription.LinkDescription;
 import us.ihmc.robotics.robotDescription.RobotDescription;
+import us.ihmc.tools.io.printing.PrintTools;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -20,7 +22,9 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Doug Stephen <a href="mailto:dstephen@ihmc.us">(dstephen@ihmc.us)</a>
@@ -53,8 +57,44 @@ public class RobotDescriptionFromURDFLoader
       {
          robotDescription = new RobotDescription(modelName);
          URDFRobot urdfRobot = (URDFRobot) coreUnmashaller.unmarshal(modelFileResourceURL);
-
          List<URDFGazebo> gazeboTags = getGazeboExtensions(modelFileResourceURL);
+
+         HashMap<String, URDFJoint> allJointsFromURDFRobot = new HashMap<>();
+         HashMap<String, URDFLink> allLinksFromURDFRobot = new HashMap<>();
+         HashMap<String, URDFMaterialGlobal> allGlobalMaterialsFromURDFRobot = new HashMap<>();
+         HashMap<String, URDFTransmission> allTransmissionFromURDFRobot = new HashMap<>();
+
+         ModelFileLoaderConversionsHelper.getGlobalLinksJointsMaterialsAndTransmissionFromURDFRobot(urdfRobot, allJointsFromURDFRobot, allLinksFromURDFRobot, allGlobalMaterialsFromURDFRobot,
+                                                                                                    allTransmissionFromURDFRobot);
+
+         for (Map.Entry<String, URDFLink> stringURDFLinkEntry : allLinksFromURDFRobot.entrySet())
+         {
+            String linkName = stringURDFLinkEntry.getKey();
+            URDFLink link = stringURDFLinkEntry.getValue();
+
+            LinkDescription linkDescription = new LinkDescription(linkName);
+
+            ArrayList<URDFVisual> linkVisuals = new ArrayList<>();
+            ArrayList<URDFCollision> linkCollisions = new ArrayList<>();
+
+            URDFInertial inertialInformationForURDFLink = ModelFileLoaderConversionsHelper.getInertialInformationAndPackVisualsAndCollisionsForURDFLink(link, linkVisuals, linkCollisions);
+            setupInertialInformationForLink(linkName, linkDescription, inertialInformationForURDFLink);
+         }
+
+         for (Map.Entry<String, URDFJoint> stringURDFJointEntry : allJointsFromURDFRobot.entrySet())
+         {
+            System.out.println(stringURDFJointEntry.getKey());
+         }
+
+         for (Map.Entry<String, URDFMaterialGlobal> stringURDFMaterialGlobalEntry : allGlobalMaterialsFromURDFRobot.entrySet())
+         {
+            System.out.println(stringURDFMaterialGlobalEntry.getKey());
+         }
+
+         for (Map.Entry<String, URDFTransmission> stringURDFTransmissionEntry : allTransmissionFromURDFRobot.entrySet())
+         {
+            System.out.println(stringURDFTransmissionEntry.getKey());
+         }
       }
       catch (Throwable e)
       {
@@ -63,6 +103,104 @@ public class RobotDescriptionFromURDFLoader
       }
 
       return robotDescription;
+   }
+
+   private void setupInertialInformationForLink(String linkName, LinkDescription linkDescription, URDFInertial inertialInformationForURDFLink)
+   {
+      if (inertialInformationForURDFLink != null)
+      {
+         URDFMass mass = inertialInformationForURDFLink.getMass();
+         URDFInertia inertia = inertialInformationForURDFLink.getInertia();
+         URDFPose origin = inertialInformationForURDFLink.getOrigin();
+
+         parseMassProperties(linkName, linkDescription, mass, origin);
+
+         parseInertiaProperties(linkName, linkDescription, inertia, origin);
+      }
+      else
+      {
+         PrintTools.warn(this, "No <intertial> tag for link " + linkName + ", setting all inertial properties to 0.0");
+         linkDescription.setMomentOfInertia(0.0, 0.0, 0.0);
+         linkDescription.setMass(0.0);
+         linkDescription.setCenterOfMassOffset(0.0, 0.0, 0.0);
+      }
+   }
+
+   private void parseInertiaProperties(String linkName, LinkDescription linkDescription, URDFInertia inertia, URDFPose origin)
+   {
+      RotationMatrix rotationMatrix = new RotationMatrix();
+      Matrix3D momentOfInertia = new Matrix3D();
+
+      if (inertia != null)
+      {
+         momentOfInertia.set(inertia.getIxx(), inertia.getIxy(), inertia.getIxz(), inertia.getIxy(), inertia.getIyy(), inertia.getIyz(), inertia.getIxz(),
+                             inertia.getIyz(), inertia.getIzz());
+
+         if (origin != null)
+         {
+            String rpyString = origin.getRpy();
+            String[] rpySplit = rpyString.split(" ");
+
+            if (rpySplit == null || rpySplit.length != 3)
+            {
+               PrintTools.warn(this, "Improperly formatted Intertial pose RPY values for link " + linkName + ", skipping transformation");
+            }
+            else
+            {
+               try
+               {
+                  double roll = Double.parseDouble(rpySplit[0]);
+                  double pitch = Double.parseDouble(rpySplit[1]);
+                  double yaw = Double.parseDouble(rpySplit[2]);
+
+                  rotationMatrix.setYawPitchRoll(yaw, pitch, roll);
+               }
+               catch (Throwable e)
+               {
+                  PrintTools.warn(this, "Improperly formatted Intertial pose RPY values for link " + linkName + ", skipping transformation");
+               }
+            }
+         }
+
+         rotationMatrix.transform(momentOfInertia);
+         linkDescription.setMomentOfInertia(momentOfInertia);
+      }
+   }
+
+   private void parseMassProperties(String linkName, LinkDescription linkDescription, URDFMass mass, URDFPose origin)
+   {
+      if (mass != null)
+      {
+         linkDescription.setMass(mass.getValue());
+      }
+
+      if (origin != null)
+      {
+         String xyzString = origin.getXyz();
+         String[] xyzSplit = xyzString.split(" ");
+
+         if (xyzSplit == null || xyzSplit.length != 3)
+         {
+            PrintTools.warn(this, "Improperly formatted Intertial pose xyz values for link " + linkName + ", skipping offset");
+         }
+         else
+         {
+            try
+            {
+               {
+                  double x = Double.parseDouble(xyzSplit[0]);
+                  double y = Double.parseDouble(xyzSplit[1]);
+                  double z = Double.parseDouble(xyzSplit[2]);
+
+                  linkDescription.setCenterOfMassOffset(x, y, z);
+               }
+            }
+            catch (Throwable e)
+            {
+               PrintTools.warn(this, "Improperly formatted Intertial pose xyz values for link " + linkName + ", skipping offset");
+            }
+         }
+      }
    }
 
    private List<URDFGazebo> getGazeboExtensions(URL modelFileResourceURL) throws XMLStreamException, IOException, JAXBException
