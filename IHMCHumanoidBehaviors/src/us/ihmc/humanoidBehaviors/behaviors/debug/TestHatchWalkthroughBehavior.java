ac9f4d35be2ec42b7c667d97e7d861846cda8de7
Tim Seyde <tseyde@ihmc.us>
2017-03-22 16:59:01 -0500

    Added 3 different speeds for passing through hatch. Custom waypoint trajectory for back foot.

--- IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/debug/TestHatchWalkthroughBehavior.java
+++ IHMCHumanoidBehaviors/src/us/ihmc/humanoidBehaviors/behaviors/debug/TestHatchWalkthroughBehavior.java
@@ -3,6 +3,7 @@ package us.ihmc.humanoidBehaviors.behaviors.debug;
 import us.ihmc.communication.packets.PacketDestination;
 import us.ihmc.communication.packets.TextToSpeechPacket;
 import us.ihmc.euclid.axisAngle.AxisAngle;
+import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.humanoidBehaviors.behaviors.AbstractBehavior;
@@ -31,9 +32,14 @@ import us.ihmc.robotics.geometry.FramePose;
 import us.ihmc.robotics.referenceFrames.ReferenceFrame;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.robotics.time.YoStopwatch;
+import us.ihmc.robotics.trajectories.TrajectoryType;
 
 public class TestHatchWalkthroughBehavior extends AbstractBehavior
 {
+   private static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+   private final RigidBodyTransform hatchToWorld = new RigidBodyTransform();
+   private final ReferenceFrame hatchFrame = ReferenceFrame.constructFrameWithUnchangingTransformFromParent("HatchFrame", worldFrame, hatchToWorld);
+   
    private final HumanoidReferenceFrames referenceFrames;
    private final DoubleYoVariable swingTime = new DoubleYoVariable("BehaviorSwingTime", registry);
    private final DoubleYoVariable sleepTime = new DoubleYoVariable("BehaviorSleepTime", registry);
@@ -44,15 +50,17 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
 
    private final YoStopwatch timer;
    
-   double armTrajectoryTime = 4.0;
+   double armTrajectoryTime; // was 4.0
    double leftArmGoalPosition[] = new double[] { -1.57, -0.51, 0.0, 2.0, 0.0, 0.0, 0.0 };
    double rightArmGoalPosition[] = new double[] { 1.57, 0.51, 0.0, -2.0, 0.0, 0.0, 0.0 };
    boolean madeFirstStepThroughHatch = false;
    boolean madeSecondStepThroughHatch = false;
    boolean robotConfigurationInitialized = false;
+   boolean changedRobotConfiguration = false;
    double counterHelper = 0;
    private final ConcurrentListeningQueue<FootstepStatus> footstepStatusQueue = new ConcurrentListeningQueue<FootstepStatus>(10);
 
+   private final int version = 2;
 
    public TestHatchWalkthroughBehavior(CommunicationBridgeInterface communicationBridge, HumanoidReferenceFrames referenceFrames, DoubleYoVariable yoTime)
    {
@@ -65,6 +73,21 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
       stepLength.set(0.3);
 
       timer = new YoStopwatch(yoTime);
+      
+      switch (version)
+      {
+      case 0:
+         armTrajectoryTime = 4.0;
+         break;
+      case 1:
+         armTrajectoryTime = 2.0;
+         break;
+      case 2:
+         armTrajectoryTime = 1.0;
+         break;
+      default:
+         break;
+      }
    }
    
    @Override
@@ -83,6 +106,11 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
       if(!madeFirstStepThroughHatch)
       {  
          makeFirstStepThroughHatchOpening();
+         counterHelper = timer.totalElapsed();
+      }
+      else if(!changedRobotConfiguration && (timer.totalElapsed() > (counterHelper + 1.5 * armTrajectoryTime)))
+      {
+         changeRobotConfiguration();
          counterHelper = timer.totalElapsed();
       }
       else if(!madeSecondStepThroughHatch && (timer.totalElapsed() > (counterHelper + 1.5 * armTrajectoryTime)))
@@ -124,9 +152,31 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
    
    public void makeFirstStepThroughHatchOpening()
    {
+      //Do this when restructuring code
+//      FramePoint startPointInFrontOfHatch = new FramePoint(hatchFrame, -0.1, 0.0, 0.0);
+//      startPointInFrontOfHatch.changeFrame(worldFrame);
+      
+      // Not present for 4.0 time
+      if(version == 1 || version == 2)
+      {
+         AxisAngle chestOrientationAA = new AxisAngle(0.0, 1.0, 0.0, Math.toRadians(10.0));
+         Quaternion chestOrientation = new Quaternion(chestOrientationAA);
+         ChestTrajectoryMessage chestOrientationTrajectoryMessage = new ChestTrajectoryMessage(armTrajectoryTime, chestOrientation);
+         sendPacket(chestOrientationTrajectoryMessage);
+      }
+      
       AxisAngle pelvisOrientationAA = new AxisAngle(0.0, 1.0, 0.0, Math.toRadians(0.0)); //was 10.0, (might have) worked
       Quaternion pelvisOrientation = new Quaternion(pelvisOrientationAA);
-      PelvisOrientationTrajectoryMessage pelvisOrientationTrajectoryMessage = new PelvisOrientationTrajectoryMessage(1.5*armTrajectoryTime, pelvisOrientation);
+      double factor = 1.5;
+      if(version == 1)
+      {
+         factor = 2.5;
+      }
+      else if(version == 2)
+      {
+         factor = 4.5;
+      }
+      PelvisOrientationTrajectoryMessage pelvisOrientationTrajectoryMessage = new PelvisOrientationTrajectoryMessage(factor*armTrajectoryTime, pelvisOrientation); // was 1.5
       sendPacket(pelvisOrientationTrajectoryMessage);
       
 //      ReferenceFrame pelvisFrame = referenceFrames.getPelvisFrame();
@@ -164,8 +214,49 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
       madeFirstStepThroughHatch = true;
    }
    
+   public void changeRobotConfiguration()
+   {
+      AxisAngle pelvisGoalOrientationYAA = new AxisAngle(0.0, 1.0, 0.0, Math.toRadians(10.0));
+      Quaternion pelvisGoalOrientationY = new Quaternion(pelvisGoalOrientationYAA);
+      AxisAngle pelvisGoalOrientationXAA = new AxisAngle(1.0, 0.0, 0.0, Math.toRadians(-1.0));
+      Quaternion pelvisGoalOrientationX = new Quaternion(pelvisGoalOrientationXAA);
+      
+      Quaternion pelvisGoalOrientation = new Quaternion();
+      pelvisGoalOrientation.multiply(pelvisGoalOrientationX, pelvisGoalOrientationY);
+      
+      ReferenceFrame pelvisFrame = referenceFrames.getPelvisFrame();
+      FramePose pelvisPose = new FramePose(pelvisFrame);
+      pelvisPose.setX(0.12);
+      if(version == 0)
+      {
+       pelvisPose.setZ(0.05); // present for 4.0
+      }
+      pelvisPose.changeFrame(ReferenceFrame.getWorldFrame());
+      Point3D pelvisGoalLocation = new Point3D();
+      
+      pelvisPose.getPose(pelvisGoalLocation, new Quaternion());
+
+      PelvisTrajectoryMessage pelvisTrajectoryMessage = new PelvisTrajectoryMessage(armTrajectoryTime, pelvisGoalLocation, pelvisGoalOrientation);
+      sendPacket(pelvisTrajectoryMessage);
+      
+      changedRobotConfiguration = true;
+   }
+   
    public void makeSecondStepThroughHatchOpening()
    {
+      // Not present for 4.0
+      if(version == 1 || version == 2)
+      {
+         ReferenceFrame pelvisFrame = referenceFrames.getPelvisFrame();
+         FramePose pelvisPose = new FramePose(pelvisFrame);
+         pelvisPose.setZ(0.05);
+         pelvisPose.changeFrame(ReferenceFrame.getWorldFrame());
+         Point3D pelvisGoalLocation = new Point3D();
+         pelvisPose.getPose(pelvisGoalLocation, new Quaternion());
+         PelvisHeightTrajectoryMessage pelvisHeightTrajectoryMessage = new PelvisHeightTrajectoryMessage(armTrajectoryTime, pelvisGoalLocation.getZ());
+         sendPacket(pelvisHeightTrajectoryMessage);
+      }
+      
       FootstepDataListMessage footsteps = new FootstepDataListMessage(swingTime.getDoubleValue(), transferTime.getDoubleValue());
       footsteps.setExecutionMode(ExecutionMode.OVERRIDE);
       footsteps.setDestination(PacketDestination.BROADCAST);
@@ -173,7 +264,7 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
       ReferenceFrame leftSoleFrame = referenceFrames.getSoleFrame(RobotSide.LEFT);
       FramePose stepPose = new FramePose(leftSoleFrame);
       stepPose.setX(0.58);
-      stepPose.setY(-0.08);
+      stepPose.setY(0.00);
 
       stepPose.changeFrame(ReferenceFrame.getWorldFrame());
 
@@ -183,9 +274,29 @@ public class TestHatchWalkthroughBehavior extends AbstractBehavior
 
       FootstepDataMessage footstepData = new FootstepDataMessage(RobotSide.LEFT, location, orientation);
       footstepData.setOrigin(FootstepOrigin.AT_SOLE_FRAME);
+      
+      
+      FramePose wayPointPose1 = new FramePose(leftSoleFrame);
+      wayPointPose1.setX(0.06);
+      wayPointPose1.setY(-0.06);
+      wayPointPose1.setZ(0.25);
+      wayPointPose1.changeFrame(ReferenceFrame.getWorldFrame());
+      Point3D locationWayPoint1 = new Point3D();
+      wayPointPose1.getPose(locationWayPoint1, new Quaternion());
+      
+      FramePose wayPointPose2 = new FramePose(leftSoleFrame);
+      wayPointPose2.setX(0.45);
+      wayPointPose1.setY(-0.06);
+      wayPointPose2.setZ(0.19);
+      wayPointPose2.changeFrame(ReferenceFrame.getWorldFrame());
+      Point3D locationWayPoint2 = new Point3D();
+      wayPointPose2.getPose(locationWayPoint2, new Quaternion());
+      
+      footstepData.setTrajectoryType(TrajectoryType.CUSTOM);
+      footstepData.setTrajectoryWaypoints(new Point3D[] {locationWayPoint1, locationWayPoint2});
       footsteps.add(footstepData);
       
-      footstepData.setSwingHeight(0.17);
+      //footstepData.setSwingHeight(0.19);
 
       sendPacket(footsteps);
       madeSecondStepThroughHatch = true;
