a63c2e776cde76ccd69ae49b8adbd7224a033ac3
Carson Wilber <cwilber@ihmc.us>
2017-09-01 10:13:33 -0500

    Removed LiveMeshUpdater to avoid explicit threading - now handle by an extension of AnimationTimer. Removed AtomicReference in CachedMechProvider.

--- IHMCRobotDataVisualizer/test/us/ihmc/robotDataVisualizer/graphics/LiveMeshDisplayTest.java
+++ IHMCRobotDataVisualizer/test/us/ihmc/robotDataVisualizer/graphics/LiveMeshDisplayTest.java
@@ -6,24 +6,6 @@ import static org.junit.Assert.fail;
 
 public class LiveMeshDisplayTest
 {
-   @Test public void TestLiveMeshUpdaterRequiresNonNullDisplay() {
-      try {
-         new LiveMeshUpdater(null, new NullMeshProvider());
-         fail();
-      } catch (NullPointerException npe) {
-         // pass
-      }
-   }
-
-   @Test public void TestLiveMeshUpdaterRequiresNonNullProvider() {
-      try {
-         new LiveMeshUpdater(new LiveMeshDisplay(new NullMeshProvider()), null);
-         fail();
-      } catch (NullPointerException npe) {
-         // pass
-      }
-   }
-
    @Test public void TestLiveMeshDisplayRequiresNonNullProvider() {
       try {
          new LiveMeshDisplay(null);
