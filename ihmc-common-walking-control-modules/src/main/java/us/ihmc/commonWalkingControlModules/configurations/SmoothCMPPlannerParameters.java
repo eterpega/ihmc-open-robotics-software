eb735abcf4fd1d7d2b9f15f63414de0d277748b6
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-09-21 11:52:10 -0500

    Enabling momentum based CMP calc for testing

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/SmoothCMPPlannerParameters.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/configurations/SmoothCMPPlannerParameters.java
@@ -93,7 +93,7 @@ public class SmoothCMPPlannerParameters extends ICPWithTimeFreezingPlannerParame
 
    public boolean planWithAngularMomentum()
    {
-      return false;
+      return true;
    }
 
    @Override
