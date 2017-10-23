46b9e0af93444308826b3858e28b31184171f8dc
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-10-23 18:19:02 -0500

    Fixed mesh offset bug. Working on collision points determination and their jacobians

--- ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/collisionAvoidance/RobotCollisionMeshProviderTest.java
+++ ihmc-avatar-interfaces/src/test/java/us/ihmc/avatar/collisionAvoidance/RobotCollisionMeshProviderTest.java
@@ -14,8 +14,14 @@ import us.ihmc.avatar.jointAnglesWriter.JointAnglesWriter;
 import us.ihmc.avatar.networkProcessor.kinematicsToolboxModule.KinematicsToolboxControllerTestRobots;
 import us.ihmc.commons.PrintTools;
 import us.ihmc.commons.RandomNumbers;
+import us.ihmc.euclid.referenceFrame.FramePoint3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.geometry.polytope.ConvexPolytopeConstructor;
+import us.ihmc.geometry.polytope.ConvexPolytopeConstructor.Axis;
+import us.ihmc.geometry.polytope.DCELPolytope.CollisionDetection.HybridGJKEPACollisionDetector;
 import us.ihmc.geometry.polytope.DCELPolytope.Frame.FrameConvexPolytope;
 import us.ihmc.robotModels.FullRobotModel;
 import us.ihmc.robotModels.FullRobotModelFromDescription;
@@ -69,6 +75,53 @@ public class RobotCollisionMeshProviderTest
          jointLimitUpper -= 0.5 * rangeReduction;
          joint.setQ(RandomNumbers.nextDouble(random, jointLimitLower, jointLimitUpper));
       }
+   }
+
+   @Test
+   public void testMeshCollisionsWithObjects()
+   {
+      RobotDescription sevenDoFArm = new KinematicsToolboxControllerTestRobots.SevenDoFArm();
+      JointNameMap sevenDoFArmJointNameMap = new KinematicsToolboxControllerTestRobots.SevenDoFArmJointMap();
+      FullRobotModel robotModel = new FullRobotModelFromDescription(sevenDoFArm, sevenDoFArmJointNameMap, null);
+      RobotCollisionMeshProvider meshProvider = new RobotCollisionMeshProvider(4);
+      THashMap<RigidBody, FrameConvexPolytope> collisionPolytopeMap = meshProvider.createCollisionMeshesFromRobotDescription(robotModel, sevenDoFArm);
+      RobotFromDescription scsRobot = new RobotFromDescription(sevenDoFArm);
+      FrameConvexPolytopeVisualizer viz = new FrameConvexPolytopeVisualizer(collisionPolytopeMap.size() + 1, true, scsRobot);
+      RigidBody[] rigidBodyList = ScrewTools.computeRigidBodiesAfterThisJoint(robotModel.getRootJoint());
+      for (RigidBody rigidBody : rigidBodyList)
+      {
+         if (collisionPolytopeMap.get(rigidBody) != null)
+            viz.addPolytope(collisionPolytopeMap.get(rigidBody));
+         else
+            PrintTools.debug("Getting a null for rigid body " + rigidBody.getName());
+      }
+      viz.updateNonBlocking();
+      Vector3D initialGuessDirection = new Vector3D(0.0, 0.0, 1.0);
+      HybridGJKEPACollisionDetector collisionDetector = new HybridGJKEPACollisionDetector();
+      FrameConvexPolytope obstacle = ConvexPolytopeConstructor.getFrameCapsuleCollisionMesh(new FramePoint3D(worldFrame, 0.0, 0.0, 0.5), Axis.X, 0.2, 0.05, 8);
+      viz.addPolytope(obstacle);
+      viz.updateNonBlocking();
+      Vector3D collisionVector = new Vector3D();
+      Vector3D collisionVector2 = new Vector3D();
+      Point3D pointOnObstacle = new Point3D();
+      Point3D pointOnRobot = new Point3D();
+      for (RigidBody rigidBody : rigidBodyList)
+      {
+         FrameConvexPolytope rigidBodyMesh = collisionPolytopeMap.get(rigidBody);
+         if (rigidBodyMesh != null && collisionDetector.checkCollisionBetweenTwoPolytopes(obstacle, rigidBodyMesh, initialGuessDirection))
+         {
+            PrintTools.debug("Colliding " + (rigidBody == null ? "null" : rigidBody.getName()) + " " + (rigidBodyMesh == null ? "null" : rigidBodyMesh.getNumberOfFaces()));
+            viz.updateColor(rigidBodyMesh, Color.RED);
+            collisionDetector.runEPAExpansion(obstacle, rigidBodyMesh, collisionVector);
+            collisionDetector.runEPAExpansion(obstacle, rigidBodyMesh, pointOnObstacle, pointOnRobot);
+            collisionVector2.sub(pointOnObstacle, pointOnRobot);
+            PrintTools.debug(collisionVector2.toString() + " " + collisionVector.toString());
+            viz.showCollisionVector(collisionVector);
+         }
+      }
+      //randomizeJointPositions(new Random(124815), robotModel, 0.75);
+      //new JointAnglesWriter(scsRobot, robotModel.getRootJoint(), robotModel.getOneDoFJoints()).updateRobotConfigurationBasedOnFullRobotModel();
+      viz.update();
    }
 
    @Test
