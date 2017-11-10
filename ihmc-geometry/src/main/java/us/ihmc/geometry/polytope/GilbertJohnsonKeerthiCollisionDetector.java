e0f54c57b4bec0c440c344fb7c09a35660d4cb33
Apoorv Shrivastava <ashrivastava@ihmc.us>
2017-11-09 19:54:37 -0600

    Added some more unit tests

--- ihmc-geometry/src/main/java/us/ihmc/geometry/polytope/GilbertJohnsonKeerthiCollisionDetector.java
+++ ihmc-geometry/src/main/java/us/ihmc/geometry/polytope/GilbertJohnsonKeerthiCollisionDetector.java
@@ -1,6 +1,5 @@
 package us.ihmc.geometry.polytope;
 
-import us.ihmc.commons.PrintTools;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.robotics.lists.RecyclingArrayList;
