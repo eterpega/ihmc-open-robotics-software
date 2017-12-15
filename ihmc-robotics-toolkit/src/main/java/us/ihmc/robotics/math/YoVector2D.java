54c1cd7d41dd65ea56599870e9be6a0024941784
Carson Wilber <cwilber@ihmc.us>
2017-12-15 17:06:51 -0600

    YoVector documentation and slight fix-ups

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoVector2D.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoVector2D.java
@@ -8,63 +8,92 @@ import us.ihmc.yoVariables.variable.YoDouble;
 
 public class YoVector2D implements Vector2DBasics, GeometryObject<YoVector2D>
 {
+   /** The x-component. */
    private final YoDouble x;
+   /** The y-component. */
    private final YoDouble y;
 
+   /**
+    * Creates a new yo vector using the given variables.
+    *
+    * @param xVariable the x-component variable.
+    * @param yVariable the y-component variable.
+    */
    public YoVector2D(YoDouble xVariable, YoDouble yVariable)
    {
-      this.x = xVariable;
-      this.y = yVariable;
+      x = xVariable;
+      y = yVariable;
    }
 
+   /**
+    * Creates a new yo vector, initializes its components to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoVector2D(String namePrefix, YoVariableRegistry registry)
    {
-      x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, ""), registry);
-      y = new YoDouble(YoFrameVariableNameTools.createYName(namePrefix, ""), registry);
+      this(namePrefix, "", registry);
    }
 
+   /**
+    * Creates a new yo vector, initializes its components to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param nameSuffix a string to use as the suffix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoVector2D(String namePrefix, String nameSuffix, YoVariableRegistry registry)
    {
       x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, nameSuffix), registry);
       y = new YoDouble(YoFrameVariableNameTools.createYName(namePrefix, nameSuffix), registry);
    }
    
+   /** {@inheritDoc} */
    @Override
    public void setX(double x)
    {
       this.x.set(x);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setY(double y)
    {
       this.y.set(y);
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getX()
    {
       return x.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getY()
    {
       return y.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean epsilonEquals(YoVector2D other, double epsilon)
    {
       return Vector2DBasics.super.epsilonEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean geometricallyEquals(YoVector2D other, double epsilon)
    {
       return Vector2DBasics.super.geometricallyEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void set(YoVector2D other)
    {
--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoVector3D.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoVector3D.java
@@ -8,24 +8,47 @@ import us.ihmc.yoVariables.variable.YoDouble;
 
 public class YoVector3D implements Vector3DBasics, GeometryObject<YoVector3D>
 {
+   /** The x-component. */
    private final YoDouble x;
+   /** The y-component. */
    private final YoDouble y;
+   /** The z-component. */
    private final YoDouble z;
 
+   /**
+    * Creates a new yo vector using the given variables.
+    *
+    * @param xVariable the x-component variable.
+    * @param yVariable the y-component variable.
+    * @param zVariable the z-component variable.
+    */
    public YoVector3D(YoDouble xVariable, YoDouble yVariable, YoDouble zVariable)
    {
-      this.x = xVariable;
-      this.y = yVariable;
-      this.z = zVariable;
+      x = xVariable;
+      y = yVariable;
+      z = zVariable;
    }
 
+   /**
+    * Creates a new yo vector, initializes its components to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoVector3D(String namePrefix, YoVariableRegistry registry)
    {
-      x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, ""), registry);
-      y = new YoDouble(YoFrameVariableNameTools.createYName(namePrefix, ""), registry);
-      z = new YoDouble(YoFrameVariableNameTools.createZName(namePrefix, ""), registry);
+      this(namePrefix, "", registry);
    }
 
+   /**
+    * Creates a new yo vector, initializes its components to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param nameSuffix a string to use as the suffix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry)
    {
       x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, nameSuffix), registry);
@@ -33,54 +56,63 @@ public class YoVector3D implements Vector3DBasics, GeometryObject<YoVector3D>
       z = new YoDouble(YoFrameVariableNameTools.createZName(namePrefix, nameSuffix), registry);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setX(double x)
    {
       this.x.set(x);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setY(double y)
    {
       this.y.set(y);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setZ(double z)
    {
       this.z.set(z);
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getX()
    {
       return x.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getY()
    {
       return y.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getZ()
    {
       return z.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean epsilonEquals(YoVector3D other, double epsilon)
    {
       return Vector3DBasics.super.epsilonEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean geometricallyEquals(YoVector3D other, double epsilon)
    {
       return Vector3DBasics.super.geometricallyEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void set(YoVector3D other)
    {
