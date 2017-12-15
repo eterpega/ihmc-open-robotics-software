a4d33bd54445e3c3c56c9952a3ee76a9538276d8
Carson Wilber <cwilber@ihmc.us>
2017-12-15 17:04:02 -0600

    YoPoint documentation and slight fix-ups

--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoPoint2D.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoPoint2D.java
@@ -8,63 +8,92 @@ import us.ihmc.yoVariables.variable.YoDouble;
 
 public class YoPoint2D implements Point2DBasics, GeometryObject<YoPoint2D>
 {
+   /** The x-coordinate. */
    private final YoDouble x;
+   /** The y-coordinate. */
    private final YoDouble y;
-   
+
+   /**
+    * Creates a new yo point using the given variables.
+    *
+    * @param xVariable the x-coordinate variable.
+    * @param yVariable the y-coordinate variable.
+    */
    public YoPoint2D(YoDouble xVariable, YoDouble yVariable)
    {
-      this.x = xVariable;
-      this.y = yVariable;
+      x = xVariable;
+      y = yVariable;
    }
 
+   /**
+    * Creates a new yo point, initializes its coordinates to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoPoint2D(String namePrefix, YoVariableRegistry registry)
    {
-      x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, ""), registry);
-      y = new YoDouble(YoFrameVariableNameTools.createYName(namePrefix, ""), registry);
+      this(namePrefix, "", registry);
    }
-
+   
+   /**
+    * Creates a new yo point, initializes its coordinates to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param nameSuffix a string to use as the suffix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoPoint2D(String namePrefix, String nameSuffix, YoVariableRegistry registry)
    {
       x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, nameSuffix), registry);
       y = new YoDouble(YoFrameVariableNameTools.createYName(namePrefix, nameSuffix), registry);
    }
    
+   /** {@inheritDoc} */
    @Override
    public void setX(double x)
    {
       this.x.set(x);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setY(double y)
    {
       this.y.set(y);
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getX()
    {
       return x.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getY()
    {
       return y.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean epsilonEquals(YoPoint2D other, double epsilon)
    {
       return Point2DBasics.super.epsilonEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean geometricallyEquals(YoPoint2D other, double epsilon)
    {
       return Point2DBasics.super.geometricallyEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void set(YoPoint2D other)
    {
--- ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoPoint3D.java
+++ ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/YoPoint3D.java
@@ -8,24 +8,47 @@ import us.ihmc.robotics.math.frames.YoFrameVariableNameTools;
 
 public class YoPoint3D implements Point3DBasics, GeometryObject<YoPoint3D>
 {
+   /** The x-coordinate. */
    private final YoDouble x;
+   /** The y-coordinate. */
    private final YoDouble y;
+   /** The z-coordinate. */
    private final YoDouble z;
-   
+
+   /**
+    * Creates a new yo point using the given variables.
+    *
+    * @param xVariable the x-coordinate variable.
+    * @param yVariable the y-coordinate variable.
+    * @param zVariable the z-coordinate variable.
+    */
    public YoPoint3D(YoDouble xVariable, YoDouble yVariable, YoDouble zVariable)
    {
-      this.x = xVariable;
-      this.y = yVariable;
-      this.z = zVariable;
+      x = xVariable;
+      y = yVariable;
+      z = zVariable;
+   }
+
+   /**
+    * Creates a new yo point, initializes its coordinates to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
+   public YoPoint3D(String namePrefix, YoVariableRegistry registry)
+   {
+      this(namePrefix, "", registry);
    }
    
-    public YoPoint3D(String namePrefix, YoVariableRegistry registry)
-    {
-       x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, ""), registry);
-       y = new YoDouble(YoFrameVariableNameTools.createYName(namePrefix, ""), registry);
-       z = new YoDouble(YoFrameVariableNameTools.createZName(namePrefix, ""), registry);
-    }
-    
+   /**
+    * Creates a new yo point, initializes its coordinates to zero, and registers variables
+    * to {@code registry}.
+    *
+    * @param namePrefix a unique name string to use as the prefix for child variable names.
+    * @param nameSuffix a string to use as the suffix for child variable names.
+    * @param registry the registry to register child variables to.
+    */
    public YoPoint3D(String namePrefix, String nameSuffix, YoVariableRegistry registry)
    {
       x = new YoDouble(YoFrameVariableNameTools.createXName(namePrefix, nameSuffix), registry);
@@ -33,54 +56,63 @@ public class YoPoint3D implements Point3DBasics, GeometryObject<YoPoint3D>
       z = new YoDouble(YoFrameVariableNameTools.createZName(namePrefix, nameSuffix), registry);
    }
    
+   /** {@inheritDoc} */
    @Override
    public void setX(double x)
    {
       this.x.set(x);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setY(double y)
    {
       this.y.set(y);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void setZ(double z)
    {
       this.z.set(z);
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getX()
    {
       return x.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getY()
    {
       return y.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public double getZ()
    {
       return z.getDoubleValue();
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean epsilonEquals(YoPoint3D other, double epsilon)
    {
       return Point3DBasics.super.epsilonEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public boolean geometricallyEquals(YoPoint3D other, double epsilon)
    {
       return Point3DBasics.super.geometricallyEquals(other, epsilon);
    }
 
+   /** {@inheritDoc} */
    @Override
    public void set(YoPoint3D other)
    {
