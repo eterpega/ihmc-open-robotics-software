5b7d4885edc93bee72cb3ba78e724eaf60d28bdc
InhoLee <ilee@ihmc.us>
2018-02-05 17:15:56 -0600

    TriWheelCart is cretated with caster wheel.

--- example-simulations/src/main/java/us/ihmc/exampleSimulations/cart/TriWheelCartSimulation.java
+++ example-simulations/src/main/java/us/ihmc/exampleSimulations/cart/TriWheelCartSimulation.java
@@ -6,7 +6,6 @@ import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.exampleSimulations.newtonsCradle.GroundAsABoxRobot;
 import us.ihmc.exampleSimulations.newtonsCradle.PileOfRandomObjectsRobot;
 import us.ihmc.graphicsDescription.Graphics3DObject;
-import us.ihmc.robotics.robotDescription.CollisionMasksHelper;
 import us.ihmc.simulationconstructionset.FloatingJoint;
 import us.ihmc.simulationconstructionset.Robot;
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
@@ -15,19 +14,18 @@ import us.ihmc.simulationconstructionset.physics.CollisionHandler;
 import us.ihmc.simulationconstructionset.physics.collision.DefaultCollisionHandler;
 import us.ihmc.simulationconstructionset.physics.collision.DefaultCollisionVisualizer;
 
-public class CartRobotSimulation
+public class TriWheelCartSimulation
 {
-
-   public CartRobotSimulation()
+   public TriWheelCartSimulation()
    {
       System.out.println("Hello World.");
-      
+
       double dt = 0.001;
+
+      TriWheelCartDescription robotDesription = new TriWheelCartDescription("triwheel");
       
-      Vector3D startingPoint = new Vector3D(-3.0, 0.0, 0.3);
-      
-      CartRobotDescription robotDesription = new CartRobotDescription("rollingRobot");
-      
+      Vector3D startingPoint = new Vector3D(-2.0, 0.0, robotDesription.getInitialHeight());
+
       SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
       parameters.setCreateGUI(true);
       parameters.setDataBufferSize(8000);
@@ -38,45 +36,43 @@ public class CartRobotSimulation
       staticLinkGraphics.addCoordinateSystem(0.1);
       scs.addStaticLinkGraphics(staticLinkGraphics);
 
-      CollisionMasksHelper helper = new CollisionMasksHelper();
-      int nextGroupBitMask = helper.getNextGroupBitMask();
-      
       // add group and mask for every links for robot.
 
       // create robot.
-      Robot cartRobot = scs.addRobot(robotDesription);      
-      FloatingJoint floatingJoint = (FloatingJoint) cartRobot.getRootJoints().get(0);
+      Robot triWheelRobot = scs.addRobot(robotDesription);
+      FloatingJoint floatingJoint = (FloatingJoint) triWheelRobot.getRootJoints().get(0);
       floatingJoint.setPosition(startingPoint);
-      
+
       // robot controller.
-      CartRobotController controller = new CartRobotController(cartRobot, dt);
-      cartRobot.setController(controller);
+      TriWheelCartController controller = new TriWheelCartController(triWheelRobot, dt);
+      triWheelRobot.setController(controller);
       
+
       // obstacles.
-      PileOfRandomObjectsRobot pileOfRandomObjectsRobot = new PileOfRandomObjectsRobot();
-      pileOfRandomObjectsRobot.setSizeScale(2.0);
-      pileOfRandomObjectsRobot.setGroupAndCollisionMask(0xffff, 0xffff);
-      pileOfRandomObjectsRobot.setNumberOfObjects(100);
-      pileOfRandomObjectsRobot.setXYExtents(1.0, 1.0);
-      pileOfRandomObjectsRobot.setZMinAndMax(1.0, 2.0);
-      ArrayList<Robot> robots = pileOfRandomObjectsRobot.createAndGetRobots();
-      for (Robot robot : robots)
-      {
-         scs.addRobot(robot);
-      }
-      
+//      PileOfRandomObjectsRobot pileOfRandomObjectsRobot = new PileOfRandomObjectsRobot();
+//      pileOfRandomObjectsRobot.setSizeScale(2.0);
+//      pileOfRandomObjectsRobot.setGroupAndCollisionMask(0xffff, 0xffff);
+//      pileOfRandomObjectsRobot.setNumberOfObjects(100);
+//      pileOfRandomObjectsRobot.setXYExtents(1.0, 1.0);
+//      pileOfRandomObjectsRobot.setZMinAndMax(1.0, 2.0);
+//      ArrayList<Robot> robots = pileOfRandomObjectsRobot.createAndGetRobots();
+//      for (Robot robot : robots)
+//      {
+//         scs.addRobot(robot);
+//      }
+
       // ground.
       int estimatedNumberOfContactPoints = 100;
       GroundAsABoxRobot groundAsABoxRobot = new GroundAsABoxRobot();
       groundAsABoxRobot.setFloorLength(15.0);
-      groundAsABoxRobot.setFloorWidth(5.0);
+      groundAsABoxRobot.setFloorWidth(15.0);
       groundAsABoxRobot.setEstimatedNumberOfContactPoints(estimatedNumberOfContactPoints);
       groundAsABoxRobot.setAddWalls(false);
       groundAsABoxRobot.setCollisionMask(0xff);
 
       scs.addRobot(groundAsABoxRobot.createRobot());
       scs.setGroundVisible(false);
-      
+
       // simulate.
       DefaultCollisionVisualizer collisionVisualizer = new DefaultCollisionVisualizer(100.0, 100.0, 0.01, scs, 1000);
       double coefficientOfRestitution = 0.2;
@@ -92,9 +88,9 @@ public class CartRobotSimulation
       scs.setCameraPosition(0.0, -8.0, 1.4);
       scs.startOnAThread();
    }
-   
+
    public static void main(String[] args)
    {
-      new CartRobotSimulation();
+      new TriWheelCartSimulation();
    }
 }
