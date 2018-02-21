8df2b6aa5602cd0fc8fea68befb93d093bd019cf
Sylvain Bertrand <sbertrand@ihmc.us>
2018-02-20 22:30:56 -0600

    Fixed stupid bugs.

--- ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/HumanoidMessageTools.java
+++ ihmc-humanoid-robotics/src/main/java/us/ihmc/humanoidRobotics/communication/packets/HumanoidMessageTools.java
@@ -369,7 +369,6 @@ public class HumanoidMessageTools
    {
       LegCompliancePacket message = new LegCompliancePacket();
       message.maxVelocityDeltas.add(maxVelocityDeltas);
-      ;
       message.robotSide = robotSide.toByte();
       return message;
    }
@@ -1491,7 +1490,7 @@ public class HumanoidMessageTools
    public static JointspaceTrajectoryMessage createJointspaceTrajectoryMessage(double trajectoryTime, double[] desiredJointPositions)
    {
       JointspaceTrajectoryMessage message = new JointspaceTrajectoryMessage();
-      for (int jointIndex = 0; jointIndex < message.getNumberOfJoints(); jointIndex++)
+      for (int jointIndex = 0; jointIndex < desiredJointPositions.length; jointIndex++)
          message.jointTrajectoryMessages.add().set(createOneDoFJointTrajectoryMessage(trajectoryTime, desiredJointPositions[jointIndex]));
       return message;
    }
@@ -1508,7 +1507,7 @@ public class HumanoidMessageTools
    public static JointspaceTrajectoryMessage createJointspaceTrajectoryMessage(double trajectoryTime, double[] desiredJointPositions, double[] weights)
    {
       JointspaceTrajectoryMessage message = new JointspaceTrajectoryMessage();
-      for (int jointIndex = 0; jointIndex < message.getNumberOfJoints(); jointIndex++)
+      for (int jointIndex = 0; jointIndex < desiredJointPositions.length; jointIndex++)
       {
          OneDoFJointTrajectoryMessage oneDoFJointTrajectoryMessage = createOneDoFJointTrajectoryMessage(trajectoryTime, desiredJointPositions[jointIndex]);
          oneDoFJointTrajectoryMessage.setWeight(weights[jointIndex]);
