898d9537d85096445c7151562866266426df890e
Robert <rgriffin@ihmc.us>
2018-02-25 22:25:22 -0600

    possibly switched to having stepping controller in the force control manager

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/communication/packets/QuadrupedForceControllerStatePacket.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/communication/packets/QuadrupedForceControllerStatePacket.java
@@ -1,28 +1,28 @@
 package us.ihmc.quadrupedRobotics.communication.packets;
 
-import us.ihmc.quadrupedRobotics.controller.force.QuadrupedForceControllerState;
+import us.ihmc.quadrupedRobotics.controller.force.QuadrupedForceControllerEnum;
 import us.ihmc.communication.packets.Packet;
 
 public class QuadrupedForceControllerStatePacket extends Packet<QuadrupedForceControllerStatePacket>
 {
-   private QuadrupedForceControllerState state;
+   private QuadrupedForceControllerEnum state;
 
    public QuadrupedForceControllerStatePacket()
    {
       this.state = null;
    }
 
-   public QuadrupedForceControllerStatePacket(QuadrupedForceControllerState state)
+   public QuadrupedForceControllerStatePacket(QuadrupedForceControllerEnum state)
    {
       this.state = state;
    }
 
-   public QuadrupedForceControllerState get()
+   public QuadrupedForceControllerEnum get()
    {
       return state;
    }
 
-   public void set(QuadrupedForceControllerState state){ this.state = state;}
+   public void set(QuadrupedForceControllerEnum state){ this.state = state;}
    @Override public boolean epsilonEquals(QuadrupedForceControllerStatePacket other, double epsilon)
    {
       return (this.state == other.state);
